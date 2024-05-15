dd5bcf20ee2ae45cee9229364fa72ca4d3f2c377
Orion888888 <151494847+Orion888888@users.noreply.github.com>
2024-05-14 21:08:14 -0500

    added the card stylings using the bulma framework to a starter page to build the application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import Home from './pages/Home';
 
-function App() {
+function Home() {
   return (
-    <div>
-      <Home />
+    <div className="container mt-5">
+      <div className="columns is-centered">
+        <div className="column is-half">
+          <div className="card">
+            <div className="card-content">
+              <div className="content">
+                <h1 className="title">Welcome to My Website</h1>
+                <p>This is a simple home page using React and Bulma.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default Home;
