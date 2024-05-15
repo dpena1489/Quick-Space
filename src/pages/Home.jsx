4933cde7e7e8485bb9a3d69a6a3e8101b3748057
Orion888888 <151494847+Orion888888@users.noreply.github.com>
2024-05-14 22:34:03 -0500

    added some bulma styling to the base application to have a skeleton to build off of. Can be changed in whichever form.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,22 +6,22 @@ function Home() {
     <div className="container mt-5">
       <div className="columns is-centered">
         <div className="column is-half">
-          <div className="card powder-blue-metallic">
+          <div className="card has-background-info-35" style={{ borderRadius: '50px' }}>
             <div className="card-content">
               <div className="content">
-                <h1 className="title">Welcome to Quick Space</h1>
-                <p>This is a simple home page using React and Bulma.</p>
+                <h1 className="title has-text-primary-100">Welcome to Quick Space</h1>
+                <p className="has-text-text-90">This is a simple home page using React and Bulma.</p>
                 <figure className="image is-4by3 mt-3">
                   <img
                     src={QuickSpaceLogo}
                     alt="QuickSpace Logo"
-                    style={{ opacity: '0.7' }}
+                    style={{ opacity: '0.5', borderRadius: '50px' }}
                   />
                 </figure>
                 <div className="buttons mt-3">
-                  <button className="button is-primary is-rounded custom-button animate__animated animate__fadeInLeft">Button 1</button>
-                  <button className="button is-info is-rounded custom-button animate__animated animate__fadeInUp">Button 2</button>
-                  <button className="button is-success is-rounded custom-button animate__animated animate__fadeInRight">Button 3</button>
+                  <button className="button is-warning is-rounded custom-button animate__animated animate__fadeInLeft">Button 1</button>
+                  <button className="button is-warning is-rounded custom-button animate__animated animate__fadeInUp">Button 2</button>
+                  <button className="button is-warning is-rounded custom-button animate__animated animate__fadeInRight">Button 3</button>
                   <button className="button is-warning is-rounded custom-button animate__animated animate__fadeInDown">Button 4</button>
                 </div>
               </div>
