93f63e7c247a5a8ab394d703f5528a531c040263
Orion888888 <151494847+Orion888888@users.noreply.github.com>
2024-05-16 20:28:55 -0500

    installed packages for @stripe api key usage and started the base code for the CheckoutForm.jsx.

---
+++ src/pages/CheckoutForm.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_51PHFPXLGaswMygELxrhy96BkMWlBhJx0O8rnZesaIy0AXZ4P4YXikepCZvc9mMB3idrXx34OaieN0qq8JDgK5oFJ00vG97SXTf');
+
