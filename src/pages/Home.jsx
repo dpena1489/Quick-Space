eac9ac03b1c5ceba9a5fcdc95fe1dea9b3590bfb
Snalepa <snalepa1998@gmail.com>
2024-05-20 10:02:31 -0400

    created cards for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,63 @@
 import React from 'react';
-import QuickSpaceLogo from '../../images/QuickSpace_logo2.jpg'; // Adjust the path according to your folder structure
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import QuickSpaceLogo from '../../images/QuickSpace_logo2.jpg'; // Adjust the path according to your folder structure
 
 function Home() {
   return (
-    <div className="container mt-5">
-      <div className="columns is-centered">
-        <div className="column is-half">
-          <div className="card has-background-info-35" style={{ borderRadius: '50px' }}>
-            <div className="card-content">
-              <div className="content">
-                <h1 className="title has-text-primary-100">Welcome to Quick Space</h1>
-                <p className="has-text-text-90">This is a simple home page using React and Bulma.</p>
-                <figure className="image is-4by3 mt-3">
-                  <img
-                    src={QuickSpaceLogo}
-                    alt="QuickSpace Logo"
-                    style={{ opacity: '0.5', borderRadius: '50px' }}
-                  />
-                </figure>
-                <div className="buttons mt-3">
-                  <button className="button is-warning is-rounded custom-button animate__animated animate__fadeInLeft">Button 1</button>
-                  <button className="button is-warning is-rounded custom-button animate__animated animate__fadeInUp">Button 2</button>
-                  <button className="button is-warning is-rounded custom-button animate__animated animate__fadeInRight">Button 3</button>
-                  <button className="button is-warning is-rounded custom-button animate__animated animate__fadeInDown">Button 4</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div>
+      <div>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Group Space</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+      <div>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+      <div>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Studio Space</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+      <div>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
       </div>
     </div>
   );
