efef2ff699d34c9ba036e837bb60dfa362e38dec
Snalepa <snalepa1998@gmail.com>
2024-05-20 11:08:43 -0400

    card styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,61 +6,48 @@ import '../App.css'
 // import QuickSpaceLogo from '../../images/QuickSpace_logo2.jpg'; // Adjust the path according to your folder structure
 
 // Comment I want to push to save upstream
+
+const cardArray = [
+  {
+    title: "Group Space",
+    text: "Find a space to accomodate your group. From baby showers to community meetings, find a space to host the perfect event!",
+    buttonText: "See Group Spaces"
+  },
+  {
+    title: "Card Title",
+    text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    buttonText: "See House Spaces"
+  },
+  {
+    title: "Studio Space",
+    text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    buttonText: "See House Spaces"
+  },
+  {
+    title: "Studio Space",
+    text: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+    buttonText: "See House Spaces"
+  }
+]
+
+
 function Home() {
   return (
     <div class = "home-cards">
-      <div>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Group Space</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">See Group Spaces</Button>
-          </Card.Body>
-        </Card>
-      </div>
-      <div>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">See House Spaces</Button>
-          </Card.Body>
-        </Card>
-      </div>
-      <div>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Studio Space</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">See Studio Spaces</Button>
-          </Card.Body>
-        </Card>
-      </div>
-      <div>
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">See Spaces</Button>
-          </Card.Body>
-        </Card>
-      </div>
+      {cardArray.map((card, index) => {
+        return (
+          <div id={index}>
+            <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title style={{color: 'white'}}>{card.title}</Card.Title>
+              <Card.Text>{card.text}</Card.Text>
+              <Button variant="primary">{card.buttonText}</Button>
+            </Card.Body>
+          </Card>
+          </div>
+        )
+      })}
     </div>
   );
 }
