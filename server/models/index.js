94b54209f82ac97a57bab47583570f76c4be6e85
Orion888888 <151494847+Orion888888@users.noreply.github.com>
2024-05-22 20:29:18 -0500

    added the node environment to the .eslintrc.cjs file.

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Category = require('./Category');
-const Order = require('./Booking');
+const Booking = require('./Booking');
 const Listing = require('./Listing');
 
-module.exports = { User, Listing, Category, Order };
+module.exports = { User, Listing, Category, Booking };
