79d41679b1ac8191bdac9641440b42b33e3dd26e
Orion888888 <151494847+Orion888888@users.noreply.github.com>
2024-05-28 19:11:02 -0500

    commented out strip code so that all code is ready to be connected to the front end. will be creating new branch with stripe code.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
 const { User, Listing, Category, Booking } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+// const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 
 const resolvers = {
@@ -45,38 +45,39 @@ const resolvers = {
         return await Booking.findById(_id);
       }
       throw AuthenticationError
-    },
-    checkout: async (parent, args, context) => {
-      const url = new URL(context.headers.referer).origin;
-      // We map through the list of products sent by the client to extract the _id of each item and create a new Order.
-      await Order.create({ products: args.products.map(({ _id }) => _id) });
-      const line_items = [];
-  
-      for (const product of args.products) {
-        line_items.push({
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: product.name,
-              description: product.description,
-              images: [`${url}/images/${product.image}`],
-            },
-            unit_amount: product.price * 100,
-          },
-          quantity: product.purchaseQuantity,
-        });
-      }
-  
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items,
-        mode: 'payment',
-        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url: `${url}/`,
-      });
-  
-      return { session: session.id };
-    },
+
+    },      
+    // checkout: async (parent, args, context) => {
+      //   const url = new URL(context.headers.referer).origin;
+      //   // We map through the list of products sent by the client to extract the _id of each item and create a new Order.
+      //   await Order.create({ products: args.products.map(({ _id }) => _id) });
+      //   const line_items = [];
+    
+      //   for (const product of args.products) {
+      //     line_items.push({
+      //       price_data: {
+      //         currency: 'usd',
+      //         product_data: {
+      //           name: product.name,
+      //           description: product.description,
+      //           images: [`${url}/images/${product.image}`],
+      //         },
+      //         unit_amount: product.price * 100,
+      //       },
+      //       quantity: product.purchaseQuantity,
+      //     });
+      //   }
+    
+      //   const session = await stripe.checkout.sessions.create({
+      //     payment_method_types: ['card'],
+      //     line_items,
+      //     mode: 'payment',
+      //     success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+      //     cancel_url: `${url}/`,
+      //   });
+    
+      //   return { session: session.id };
+      // },
   },
 
   // order: async (parent, { _id }, context) => {
