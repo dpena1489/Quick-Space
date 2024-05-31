2c5478f8494e7931702649dd6610b2223c1fa935
Snalepa <snalepa1998@gmail.com>
2024-05-31 13:43:14 -0400

    Navbar border

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,7 +4,8 @@ import CheckoutForm from './CheckoutForm.jsx';
 
 export default function NavBar() {
   return (
-    <header className={'flex justify-between items-center border-3 border-transparent border-b-sky-600'}>
+    <div className='border-3 border-transparent border-b-sky-600'>
+    <header className={'flex justify-between items-center'}>
     <Link to='/' className={"my-2 min-w-16 max-w-20"}>
         <img
             src={logo}
@@ -12,13 +13,15 @@ export default function NavBar() {
             style={{opacity: '0.5', borderRadius: '50px'}}
         />
     </Link>
-
+    
     <ul className='flex font-bold text-xl'>
         <li className={'mx-10'}><Link to="/profile" className={"text-white no-underline"}>Profile</Link></li>
         {/* <li className={'mx-10'}><CheckoutForm/></li> */}
         <li className={'mx-10'}><Link to="/cart" className={"text-white no-underline"}>Cart</Link></li>
         <li className={'mx-10'}><Link to="/login" className={"text-white no-underline"}>Login</Link></li>
     </ul>
+    
 </header>
+</div>
   )
 }
